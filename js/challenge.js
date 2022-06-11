4e2f10d1b0d0c4154481c7404c979d49061b5713
Jose Soto <jsoto7087@gmail.com>
2022-06-10 23:59:45 -0400

    added comments and timer

--- js/challenge.js
+++ js/challenge.js
@@ -1,15 +1,35 @@
-let timerNode = document.getElementById('counter')
+const timerNode = document.getElementById('counter')
+const form = document.querySelector('form');
+
 console.log(timerNode)
+console.log(form)
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userInput = e.target.children[0].value
+    console.log(userInput)
+    const p = document.createElement('p')
+    const ul = document.getElementById('list')
+    ul.appendChild(p);
+    p.innerText = userInput;
+    form.reset()
+})
+
+
+document.getElementById('list').addEventListener('click', (e) => {
+    e.remove()
+})
+
 
 function timerCounter() {
-
     setInterval(function () {
-        let x = 0;
-        while (x >= 0) {
-            x++;
-            let time = timerNode.innerHTML += x
-            return time;
-        }
+        let int = parseInt(timerNode.innerText);
+        int++
+        timerNode.innerText = int;
     }, 1000);
 }
 timerCounter()
+
+function handleSubmit() {
+    const submit = document.getElementById("comment-input")
+}
